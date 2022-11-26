7f6ca45253f3a90ed80bb609b8400fbbdedb7ce2
Matilde Marazzini <marazzinimatilde@gmail.com>
2022-11-26 16:01:27 +0100

    graph

--- R script/regression analysis.R
+++ R script/regression analysis.R
@@ -15,3 +15,14 @@ summary(cor)
 #T value is very big
 # we can reject the Null Hypothesis --> there is association and dependence between the two variables
 # R-squared value is small --> weak correlation
+
+ggplot(dataset, aes(x = size_5c, y = E235)) +
+  geom_smooth(method = "lm", se = T) +
+  ylab("Democracy index") +
+  xlab("City Size")
+  theme_light()
+  
+#The graph is not a flat line so there is correlation between the variables. We 
+#can notice that if the city size increases, the level of the importance
+#of the democracy decreases
+
